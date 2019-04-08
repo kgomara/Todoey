30837e03f6951a865ae5303515e76b6239e180c6
Kevin OMara <kgomara@yahoo.com>
2019-04-08 13:55:58 -0700

    Fix ToDoListViewController.swift

--- Todoey/ToDoListViewController.swift
+++ Todoey/ToDoListViewController.swift
@@ -10,7 +10,7 @@ import UIKit
 
 class ToDoListViewController: UITableViewController {
     
-    let itemArray  = ["Find Mike", "Buy Eggos", "Destroy Demogorgon", ]
+    var itemArray  = ["Find Mike", "Buy Eggos", "Destroy Demogorgon", ]
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -45,5 +45,26 @@ class ToDoListViewController: UITableViewController {
         
         tableView.deselectRow(at: indexPath, animated: true)
     }
+    
+    //MARK - Add New Items
+    @IBAction func addButtonPressed(_ sender: UIBarButtonItem) {
+        
+        var textField = UITextField()
+        
+        let alert = UIAlertController(title: "Add New Todoey", message: "", preferredStyle: .alert)
+        let action = UIAlertAction(title: "Add Item", style: .default) { (action) in
+            self.itemArray.append(textField.text ?? "New Item")
+            self.tableView.reloadData()
+        }
+        
+        alert.addTextField { (alertTextField) in
+            alertTextField.placeholder = "Enter item title"
+            textField = alertTextField
+        }
+        
+        alert.addAction(action)
+        present(alert, animated: true, completion: nil)
+    }
+    
 }
 
